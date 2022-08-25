252ec8acae3ab31377f45bae407da0072b19e5f0
Abhishek Sisodiya <sisodiya2421@gmail.com>
2022-08-26 00:47:07 +0530

    feat(backend): complete product scraping

--- scrape.py
+++ scrape.py
@@ -8,6 +8,10 @@ import logging
 class Scrapper():
     def __init__(self):
         self.img_urls = []
+        self.names = []
+        self.brands = []
+        self.prices = []
+        self.ratings = []
         self.driver = webdriver.Firefox()
 
     def scrape(self, url):
@@ -20,13 +24,33 @@ class Scrapper():
         time.sleep(4)
 
         try:
-            actual_imgs = self.driver.find_elements(
-                By.CSS_SELECTOR, 'img.img-responsive')
-            for actual_img in actual_imgs:
-                if actual_img.get_attribute('src') and 'https' in actual_img.get_attribute('src'):
-                    self.img_urls.append(actual_img.get_attribute('src'))
+            products = self.driver.find_elements(By.CLASS_NAME, 'product-base')
+            for product in products:
+                try:
+                    name = product.find_element(
+                        By.CSS_SELECTOR, 'h4.product-product').text
+                    brand = product.find_element(
+                        By.CSS_SELECTOR, 'h3.product-brand').text
+                    img_url = product.find_element(
+                        By.CSS_SELECTOR, 'img.img-responsive').get_attribute('src')
+                    rating = product.find_element(
+                        By.CSS_SELECTOR, 'div.product-ratingsContainer > span').text
+                    try:
+                        price = product.find_element(
+                            By.CLASS_NAME, 'product-discountedPrice').text
+                    except:
+                        price = product.find_element(
+                            By.CLASS_NAME, 'product-price').text
+                except NoSuchElementException:
+                    continue
+                self.names.append(name)
+                self.brands.append(brand)
+                self.prices.append(price)
+                self.img_urls.append(img_url)
+                self.ratings.append(rating)
         except ElementNotInteractableException as err:
             logging.exception(f"Error occured while extraction : {err}")
+
         try:
             next_page = self.driver.find_element(
                 By.CSS_SELECTOR, 'li.pagination-next > a').get_attribute('href')
@@ -35,6 +59,10 @@ class Scrapper():
             next_page = None
 
         scrapped_data = {
+            'product': self.names,
+            'brand': self.brands,
+            'price': self.prices,
+            'rating': self.ratings,
             'img_url': self.img_urls,
         }
         # return next page url and extracted data from Myntra
